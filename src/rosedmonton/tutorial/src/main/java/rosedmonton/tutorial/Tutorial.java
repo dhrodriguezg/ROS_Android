6455cbf513c591465b65627150567d82b5c1d2ce
Diego Rodriguez <dh.rodriguez25@uniandes.edu.co>
2015-11-10 18:55:24 -0700

    ROS Activity Enabled

--- src/rosedmonton/tutorial/src/main/java/rosedmonton/tutorial/Tutorial.java
+++ src/rosedmonton/tutorial/src/main/java/rosedmonton/tutorial/Tutorial.java
@@ -1,15 +1,32 @@
 package rosedmonton.tutorial;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class Tutorial extends Activity
+import org.ros.android.RosActivity;
+import org.ros.node.NodeMainExecutor;
+
+import java.net.URI;
+
+public class Tutorial extends RosActivity
 {
+    protected Tutorial(String notificationTicker, String notificationTitle) {
+        super(notificationTicker, notificationTitle);
+    }
+
+    protected Tutorial(String notificationTicker, String notificationTitle, URI customMasterUri) {
+        super(notificationTicker, notificationTitle, customMasterUri);
+    }
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    protected void init(NodeMainExecutor nodeMainExecutor) {
+
     }
 }

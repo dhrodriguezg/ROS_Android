03267db965d960fc0ebe3ef8d980f93981ac0985
Diego Rodriguez <dh.rodriguez25@uniandes.edu.co>
2015-11-11 16:49:19 -0700

    Publisher setup

--- src/rosedmonton/tutorial/src/main/java/rosedmonton/tutorial/TutorialNode.java
+++ src/rosedmonton/tutorial/src/main/java/rosedmonton/tutorial/TutorialNode.java
@@ -4,11 +4,19 @@ import org.ros.namespace.GraphName;
 import org.ros.node.ConnectedNode;
 import org.ros.node.Node;
 import org.ros.node.NodeMain;
+import org.ros.node.topic.Publisher;
+
+import std_msgs.Int32;
 
 /**
  * Created by dhrodriguezg on 11/11/15.
  */
 public class TutorialNode implements NodeMain {
+
+    private Publisher<Int32> publisher = null;
+
+    public TutorialNode(){
+    }
 
     @Override
     public GraphName getDefaultNodeName() {
@@ -17,7 +25,7 @@ public class TutorialNode implements NodeMain {
 
     @Override
     public void onStart(ConnectedNode connectedNode) {
-
+        publisher = connectedNode.newPublisher("/android/integer_dr", Int32._TYPE);
     }
 
     @Override
@@ -33,5 +41,13 @@ public class TutorialNode implements NodeMain {
     @Override
     public void onError(Node node, Throwable throwable) {
 
+    }
+
+    public void publishData(int data, int numMessages){
+        for (int n=0; n < numMessages; n++){
+            Int32 int32 = publisher.newMessage();
+            int32.setData(data);
+            publisher.publish(int32);
+        }
     }
 }

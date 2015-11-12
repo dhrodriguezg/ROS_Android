4cb3c6c90e89b8389d820f3b1c3bbb093563e49e
Diego Rodriguez <dh.rodriguez25@uniandes.edu.co>
2015-11-11 18:13:59 -0700

    Connecting the components

--- src/rosedmonton/tutorial/src/main/java/rosedmonton/tutorial/Tutorial.java
+++ src/rosedmonton/tutorial/src/main/java/rosedmonton/tutorial/Tutorial.java
@@ -1,14 +1,23 @@
 package rosedmonton.tutorial;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import org.ros.address.InetAddressFactory;
 import org.ros.android.RosActivity;
+import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMainExecutor;
 
 import java.net.URI;
 
 public class Tutorial extends RosActivity
 {
+
+    private EditText userNumber;
+    private Button sendButton;
+    private TutorialNode tutorialNode;
 
     public Tutorial() {
         super("Tutorial", "Tutorial", URI.create("http://192.168.1.121:11311"));
@@ -20,10 +29,27 @@ public class Tutorial extends RosActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        userNumber = (EditText) findViewById(R.id.userNumber);
+        sendButton = (Button) findViewById(R.id.sendButton);
+        tutorialNode = new TutorialNode();
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s_data = userNumber.getText().toString();
+                int i_data = Integer.parseInt(s_data);
+                tutorialNode.publishData(i_data, 3);
+            }
+        });
+
     }
 
     @Override
     protected void init(NodeMainExecutor nodeMainExecutor) {
+        NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(
+                InetAddressFactory.newNonLoopback().getHostAddress(), getMasterUri());
 
+        nodeMainExecutor.execute(tutorialNode, nodeConfiguration.setNodeName("android/tutorial_node"));
     }
 }
